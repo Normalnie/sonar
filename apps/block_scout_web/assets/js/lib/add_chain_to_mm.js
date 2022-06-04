d02f502dc8e2e33ff5a8d236af145564d9d26e21
mainnet-pat <mainnet_pat@protonmail.com>
2022-06-04 14:34:14 +0000

    Workaround for undefined webpack process envs

--- apps/block_scout_web/assets/js/lib/add_chain_to_mm.js
+++ apps/block_scout_web/assets/js/lib/add_chain_to_mm.js
@@ -3,28 +3,28 @@ import 'bootstrap'
 export async function addChainToMM ({ btn }) {
   try {
     const chainID = await window.ethereum.request({ method: 'eth_chainId' })
-    const chainIDFromEnvVar = parseInt(process.env.CHAIN_ID)
+    const chainIDFromEnvVar = parseInt(process.env.CHAIN_ID || "10000")
     const chainIDHex = chainIDFromEnvVar && `0x${chainIDFromEnvVar.toString(16)}`
-    const blockscoutURL = location.protocol + '//' + location.host + process.env.NETWORK_PATH
+    const blockscoutURL = location.protocol + '//' + location.host + (process.env.NETWORK_PATH || '/')
     if (chainID !== chainIDHex) {
       await window.ethereum.request({
         method: 'wallet_addEthereumChain',
         params: [{
           chainId: chainIDHex,
-          chainName: process.env.SUBNETWORK,
+          chainName: process.env.SUBNETWORK || "SmartBCH",
           nativeCurrency: {
-            name: process.env.COIN_NAME,
-            symbol: process.env.COIN_NAME,
+            name: process.env.COIN_NAME || "BCH",
+            symbol: process.env.COIN_NAME || "BCH",
             decimals: 18
           },
-          rpcUrls: [process.env.JSON_RPC],
+          rpcUrls: [process.env.JSON_RPC || "https://smartbch.fountainhead.cash/mainnet"],
           blockExplorerUrls: [blockscoutURL]
         }]
       })
     } else {
       btn.tooltip('dispose')
       btn.tooltip({
-        title: `You're already connected to ${process.env.SUBNETWORK}`,
+        title: `You're already connected to ${process.env.SUBNETWORK || "SmartBCH"}`,
         trigger: 'click',
         placement: 'bottom'
       }).tooltip('show')
